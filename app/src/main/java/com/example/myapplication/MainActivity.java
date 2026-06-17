f74ba250c78c9ef01dc16ba910be216452ab2eaf
vitor <vitorhugovieira3397@gmail.com>
2026-06-17 09:53:26 -0300

    slideShow

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,37 +2,54 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import java.lang.reflect.Array;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button b;
-    int contador=0;
+    Button bntAvancar, bntVoltar;
+    ImageView imageView;
 
+    Integer imagens[] = new Integer[]{
+            R.drawable.cachorro,
+            R.drawable.gardem,
+            R.drawable.happy,
+            R.drawable.patinho,
+            R.drawable.porquinho
+    };
+    int posicao = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b = findViewById(R.id.button);
-        TextView tv = findViewById(R.id.tv);
 
+        bntAvancar=findViewById(R.id.buttonAvancar);
+        bntVoltar=findViewById(R.id.buttonVoltar);
+        imageView=findViewById(R.id.imageView);
+        imageView.setImageResource(imagens[posicao]);
 
-        b.setOnClickListener(v -> {
-            contador++;
-            Random random= new Random();
-            int r = random.nextInt(100);
-            tv.setText(Integer.toString(r));
-
+        bntAvancar.setOnClickListener(v -> {
+            if (posicao==imagens.length-1){
+                posicao = 0;
+            }else{
+                posicao++;
+            }
+            imageView.setImageResource(imagens[posicao]);
         });
 
+        bntVoltar.setOnClickListener(v -> {
+            if (posicao < 1){
+                posicao = 4;
+            }else{
+                posicao--;
+            }imageView.setImageResource(imagens[posicao]);
+        });
     }
 }
