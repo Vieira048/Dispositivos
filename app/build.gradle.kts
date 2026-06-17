e15d7ae3d849cf3c982576f4d19053ea1bac678b
vitor <vitorhugovieira3397@gmail.com>
2026-06-17 10:24:31 -0300

    contador

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.myapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.myapp"
--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapp;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,17 +13,22 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     Button b;
+    int contador=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        getString(R.string.app_name);
 
-        b=findViewById(R.id.button);
+        b = findViewById(R.id.button);
+        TextView tv = findViewById(R.id.tv);
+
+
         b.setOnClickListener(v -> {
-            b.setText("DALE");
+            contador++;
+            tv.setText(Integer.toString(contador));
+
         });
+
     }
 }
